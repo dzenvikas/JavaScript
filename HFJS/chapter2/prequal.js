b971848bec6d035127d79ab49c2d23db8ebe24e7
vikaskumar94 <vikaskumar.vsk@gmail.com>
2017-09-19 11:50:03 +0530

    update

--- HFJS/chapter2/prequal.js
+++ HFJS/chapter2/prequal.js
@@ -9,6 +9,29 @@ var taxi = {
 	milage: 281341
 };
 
+// fiat car object
+var fiat = {
+	make: "Fiat",
+	model: "500",
+	year: 1957,
+	color: "Medium Blue",
+	passangers: 2,
+	convertible: false,
+	milage: 88000
+};
+
+// cadillac car object
+var cadi = {
+	make: "GM",
+	model: "Cadillac",
+	year: 1955,
+	color: "tan",
+	passangers: 5,
+	convertible: false,
+	milage: 12892
+};
+
+// function to check qualification of the car
 function prequal(car) {
 	if(car.milage > 10000){
 		return false;
@@ -20,9 +43,28 @@ function prequal(car) {
 
 
 var worthALook = prequal(taxi);
+// console.log(worthALook);
 
 if(worthALook) {
 	console.log("You gotta check out this " + taxi.make + " " + taxi.model);
 } else{
 	console.log("You should really pass on the " + taxi.make + " " + taxi.model);
+}
+
+
+var worthALook = prequal(cadi);
+// console.log(worthALook);
+
+if(worthALook){
+	console.log("You gotta check this out " + cadi.make + " " + cadi.model);
+} else{
+	console.log("You should really pass on the " + cadi.make + " " + cadi.model);
+}
+
+var worthALook = prequal(fiat);
+// console.log(worthALook);
+if(worthALook){
+	console.log("You gotta check this out " + fiat.make + " " + fiat.model);
+} else{
+	console.log("You should really pass on the " + fiat.make + " " + fiat.model);
 }
